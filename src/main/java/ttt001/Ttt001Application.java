5ccaa18488b050b2605b2e55284354e5bdf25d97
Matt Cadorette <mcadorette@gmail.com>
2018-10-12 14:38:24 -0400

    Initial commit from Atomist

---
+++ src/main/java/ttt001/Ttt001Application.java
@@ -0,0 +1,12 @@
+package ttt001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ttt001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ttt001Application.class, args);
+	}
+}
---
+++ src/test/java/ttt001/Ttt001ApplicationTests.java
@@ -0,0 +1,16 @@
+package ttt001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ttt001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
